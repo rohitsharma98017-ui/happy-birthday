8c1ddc249aa09ae17b0c4e52284849e2c500b08c
Rohit Sharma <rohrt@example.com>
2026-04-22 19:29:13 +0530

    Fix incorrect module path in API handler

--- api/index.js
+++ api/index.js
@@ -1,6 +1,6 @@
 // Vercel API handler - This file is used when deployed to Vercel
 // It exports the Express app as a serverless function
 
-const app = require('../../server');
+const app = require('../server');
 
 module.exports = app;
